1ce0af10537b905ddeb463e423db408692a23598
YauheniStash <stashenin1983.doc@gmail.com>
2019-07-15 11:13:53 -0500

    n

--- src/main/java/PageRepository/Merging2.java
+++ src/main/java/PageRepository/Merging2.java
@@ -1,5 +1,15 @@
 package PageRepository;
 
-public class Merging2 {
+import org.testng.annotations.Test;
 
+public class Merging2 {
+@Test
+public void merging2() {
+	
+}
+
+@Test
+public void merging3() {
+	
+}
 }
